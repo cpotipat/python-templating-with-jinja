822c605dcf9e0d518da62b304eb961dba73984ea
cpotipat <namsom.chitchanok@gmail.com>
2021-05-14 11:00:49 -0700

    Create dynamic HTML pages

--- server.py
+++ server.py
@@ -7,20 +7,25 @@ app = Flask(__name__)
 
 def gender_of_name(name):
     response = requests.get(f"https://api.genderize.io?name={name}")
-    return response.json()
+    return response.json()["gender"]
 
 
 def age_of_name(name):
     response = requests.get(f"https://api.agify.io?name={name}")
-    return response.json()
+    return response.json()["age"]
 
 
 @app.route("/")
 def home():
     current_year = datetime.datetime.now().year
-    gender = gender_of_name("peter")["gender"]
-    age = age_of_name("peter")["age"]
-    return render_template("index.html", gender=gender, age=age, year=current_year)
+    return render_template("index.html", year=current_year)
+
+
+@app.route('/guess/<name>')
+def guess_by_name(name):
+    gender = gender_of_name(name)
+    age = age_of_name(name)
+    return render_template("guess.html", name=name, gender=gender, age=age)
 
 
 if __name__ == "__main__":
