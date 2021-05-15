df63c8837036bff96df6171ffc3f31ee9b5cea49
cpotipat <namsom.chitchanok@gmail.com>
2021-05-14 17:08:43 -0700

    Generating blog urls with Flask, and refactoring code

--- server.py
+++ server.py
@@ -1,38 +1,30 @@
 from flask import Flask, render_template
+from post import Post
 import datetime
 import requests
 
+posts = requests.get("https://api.npoint.io/6fb3de985ecf531f828f").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+
 app = Flask(__name__)
-
-
-def gender_of_name(name):
-    response = requests.get(f"https://api.genderize.io?name={name}")
-    return response.json()["gender"]
-
-
-def age_of_name(name):
-    response = requests.get(f"https://api.agify.io?name={name}")
-    return response.json()["age"]
 
 
 @app.route("/")
 def home():
     current_year = datetime.datetime.now().year
-    return render_template("index.html", year=current_year)
+    return render_template("index.html", year=current_year, posts=post_objects)
 
 
-@app.route("/guess/<name>")
-def guess_by_name(name):
-    gender = gender_of_name(name)
-    age = age_of_name(name)
-    return render_template("guess.html", name=name, gender=gender, age=age)
-
-
-@app.route("/blog")
-def get_blog():
-    blog_url = "https://api.npoint.io/6fb3de985ecf531f828f"
-    all_posts = requests.get(blog_url).json()
-    return render_template("blog.html", posts=all_posts)
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
